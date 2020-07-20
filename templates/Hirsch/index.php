3c0c0a0fc97af5c64cfd03390b57220b68af0447
Sven Nolting <nolting@hochwarth-it.de>
2020-07-20 09:02:43 +0200

    Korrektur Ruhetagsbestellung

--- templates/Hirsch/index.php
+++ templates/Hirsch/index.php
@@ -13,21 +13,26 @@ $i = 0;
     <?php
     foreach ($displayData
 
-             as $data):
-        ?>
-        <?php if ($i == 1): ?>
-</div><div class="row"><h2>Kommende Tagesessen</h2>
+    as $data):
+    $isRuhetag = strpos(strtolower($data['gericht']), 'ruhetag') !== false;
+    ?>
+    <?php if ($i == 1): ?>
+</div>
+<div class="row"><h2>Kommende Tagesessen</h2>
     <?php endif; ?>
-        <div class='col <?= ($i < 1 || $this->request->is('mobile')) ? 's12' : 's4' ?>'>
-            <div class="card <?= ($i < 1) ? "light-green" : "light-blue" ?> lighten-4">
+    <div class='col <?= ($i < 1 || $this->request->is('mobile')) ? 's12' : 's4' ?>'>
+        <div class="card <?= ($i < 1) ? "light-green" : "light-blue" ?> lighten-4">
 
-                <div class="card-content">
+            <div class="card-content">
                     <span
-                        class="card-title <?= ($i < 1) ? "activator" : "" ?> direction-left">Tagesessen <?= $data['date']->format('d.m.Y') ?><?= ($i < 1) ? '<i class="material-icons right">more_vert</i>' : "" ?></span>
-                    <p><?= $data['gericht'] ?></p><br>
+                        class="card-title <?= ($i < 1) ? "activator" : "" ?> direction-left">Tagesessen <?= $data['date']->format('d.m.Y') ?><?= ($i < 1 && !$isRuhetag) ? '<i class="material-icons right">more_vert</i>' : "" ?></span>
+                <p><?= $data['gericht'] ?></p><br>
+                <?php if (!$isRuhetag): ?>
                     <p class="chip"><span class="teal-text"><?= $this->Number->currency(3.5); ?></span></p>
+                <?php endif; ?>
 
-                </div>
+            </div>
+            <?php if (!$isRuhetag): ?>
                 <?php if ($i < 1): ?>
                     <div class="card-reveal">
                         <span class="card-title direction-left">Hirsch to Go<i
@@ -39,15 +44,14 @@ $i = 0;
                         </ul>
                     </div>
                     <div class="card-action">
-                        <?php if (strpos(strtolower($data['gericht']), 'ruhetag') !== false): ?>
-                            <?= ((new Time())->hour < 10 || ((new Time())->hour == 10 && (new Time())->minute <= 45)) ? $this->Form->postButton("Tagesessen bestellen", ['controller' => 'hirsch', 'action' => 'order', $data['gericht']], ['class' => 'btn center-align waves-purple waves-effect']) : "Die heutigen Bestellungen sind geschlossen" ?>
-                        <?php endif; ?>
+                        <?= ((new Time())->hour < 10 || ((new Time())->hour == 10 && (new Time())->minute <= 45)) ? $this->Form->postButton("Tagesessen bestellen", ['controller' => 'hirsch', 'action' => 'order', $data['gericht']], ['class' => 'btn center-align waves-purple waves-effect']) : "Die heutigen Bestellungen sind geschlossen" ?>
                     </div>
                 <?php endif; ?>
-            </div>
+            <?php endif; ?>
         </div>
-        <?php
-        $i++;
+    </div>
+    <?php
+    $i++;
     endforeach;
     ?>
 </div>
