ee24f160269f3c9f7bcc8f0569276479aa738755
Sven Nolting <nolting@hochwarth-it.de>
2020-07-29 15:31:12 +0200

    gogogo.sh angepasst

--- gogogo.sh
+++ gogogo.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-cd /var/www/vhosts/rindula.de/hirsch.rindula.de/ || exit
+if [ "$HOSTNAME" = "rindula.de" ]; then
+    cd /var/www/vhosts/rindula.de/hirsch.rindula.de/ || exit
 
-# Composer Update
-/opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-suggest
+    # Composer Update
+    /opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar install --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-suggest
 
-# Cake
-## Datenbank Migration
-/opt/plesk/php/7.2/bin/php ./bin/cake.php migrations migrate
+    # Cake
+    ## Datenbank Migration
+    /opt/plesk/php/7.2/bin/php ./bin/cake.php migrations migrate
 
-## Cache leeren
-/opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
+    ## Cache leeren
+    /opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
+else
+    git reset --hard
+    git pull
+    composer install --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-suggest
+    chmod +x bin/cake
+    bin/cake migrations migrate
+    bin/cake cache clear_all
+fi
