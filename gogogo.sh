3ea7a51c864d2b0f1dbd5d4aab11031f621b48de
Sven Nolting <nolting@hochwarth-it.de>
2020-08-04 16:21:43 +0200

    gogogo.sh angepasst

--- gogogo.sh
+++ gogogo.sh
@@ -17,6 +17,13 @@ else
     composer install --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-suggest
     chmod +x bin/cake
     chmod +x gogogo.sh
+
+    echo '' >> webroot/.htaccess
+    echo 'AuthType Basic' >> webroot/.htaccess
+    echo 'AuthName "Bestellungen' >> webroot/.htaccess
+    echo 'AuthUserFile /usr/www/users/hochwa/hirsch/.htpasswd' >> webroot/.htaccess
+    echo 'Require valid-user' >> webroot/.htaccess
+
     bin/cake migrations migrate
     bin/cake cache clear_all
 fi
