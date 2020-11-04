d18b4d53aaf975aff0dee94b979177954ba7023d
Sven Nolting <nolting@hochwarth-it.de>
2020-11-04 10:50:19 +0100

    Slider eingefügt

--- src/Controller/HirschController.php
+++ src/Controller/HirschController.php
@@ -116,28 +116,28 @@ class HirschController extends AppController
                                 $daysAdd = 0;
                                 switch ($dow) {
                                     case 1:
-                                        preg_match('/M\s*o\s*n\s*t\s*a\s*g[^a-zA-Z0-9\-]*-?([\d\w]{1,3}[^\d\-]+)/', $text, $matches);
+                                        preg_match('/M\s*o\s*n\s*t\s*a\s*g[^a-zA-Z0-9\-]+([^\d\n]*)/', $text, $matches);
                                         $displayData[] = ['gericht' => trim($matches[1]), 'date' => (new Date())];
                                     case 2:
-                                        preg_match('/D\s*i\s*e\s*n\s*s\s*t\s*a\s*g[^a-zA-Z0-9\-]*-?([\d\w]{1,3}[^\d\-]+)/', $text, $matches);
+                                        preg_match('/D\s*i\s*e\s*n\s*s\s*t\s*a\s*g[^a-zA-Z0-9\-]*([\d\w]{1,3}[^\d]+)/', $text, $matches);
                                         if ($dow < 2) {
                                             $daysAdd++;
                                         }
                                         $displayData[] = ['gericht' => trim($matches[1]), 'date' => (new Date("+" . $daysAdd . " days"))];
                                     case 3:
-                                        preg_match('/M\s*i\s*t\s*t\s*w\s*o\s*c\s*h[^a-zA-Z0-9\-]*-?([\d\w]{1,3}[^\d\-]+)/', $text, $matches);
+                                        preg_match('/M\s*i\s*t\s*t\s*w\s*o\s*c\s*h[^a-zA-Z0-9\-]*([\d\w]{1,3}[^\d]+)/', $text, $matches);
                                         if ($dow < 3) {
                                             $daysAdd++;
                                         }
                                         $displayData[] = ['gericht' => trim($matches[1]), 'date' => (new Date("+" . $daysAdd . " days"))];
                                     case 4:
-                                        preg_match('/D\s*o\s*n\s*n\s*e\s*r\s*s\s*t\s*a\s*g[^a-zA-Z0-9\-]*-?([\d\w]{1,3}[^\d\-]+)/', $text, $matches);
+                                        preg_match('/D\s*o\s*n\s*n\s*e\s*r\s*s\s*t\s*a\s*g[^a-zA-Z0-9\-]*([\d\w]{1,3}[^\d]+)/', $text, $matches);
                                         if ($dow < 4) {
                                             $daysAdd++;
                                         }
                                         $displayData[] = ['gericht' => trim($matches[1]), 'date' => (new Date("+" . $daysAdd . " days"))];
                                     case 5:
-                                        preg_match('/F\s*r\s*e\s*i\s*t\s*a\s*g[^a-zA-Z0-9\-]*-?([\d\w]{1,3}[^\d\-]+)/', $text, $matches);
+                                        preg_match('/F\s*r\s*e\s*i\s*t\s*a\s*g[^a-zA-Z0-9\-]*([\d\w]{1,3}[^\d]+)/', $text, $matches);
                                         if ($dow < 5) {
                                             $daysAdd++;
                                         }
