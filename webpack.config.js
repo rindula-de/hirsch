a830a4f91fa3fd19a31d035204807ce1132d1087
Sven Nolting <nolting@hochwarth-it.de>
2021-11-17 10:33:24 +0100

    Bestellungen via API laden

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ Encore
  * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
  */
 .addEntry('app', './assets/app.js')
-    .addEntry('menu', './assets/menu.js')
+.addEntry('menu', './assets/menu.js')
+.addEntry('orders', './assets/orders.js')
 
 // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
 .enableStimulusBridge('./assets/controllers.json')
