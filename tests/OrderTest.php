3ee61914f245cec5cbcfe2f24ca3452c69d75fff
Sven Nolting <Rindula@users.noreply.github.com>
2021-12-09 10:35:29 +0100

    Apply fixes from StyleCI (#422)

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -9,41 +9,41 @@ class OrderTest extends WebTestCase
     public function testOrdering(): void
     {
         $client = static::createClient();
-        
+
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
         $form = $crawler->selectButton('order[submit]')->form();
         $client->submit($form, [
             'order[orderedby]' => 'Max Mustermann',
-            'order[note]' => '']);
+            'order[note]'      => '', ]);
         $this->assertResponseRedirects('/zahlen-bitte/', 302);
-        
+
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
         $form = $crawler->selectButton('order[submit]')->form();
         $client->submit($form, [
             'order[orderedby]' => 'Max Mustermann',
-            'order[note]' => '+ Pommes']);
+            'order[note]'      => '+ Pommes', ]);
         $this->assertResponseRedirects('/zahlen-bitte/', 302);
-        
+
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
         $form = $crawler->selectButton('order[submit]')->form();
         $client->submit($form, [
             'order[orderedby]' => '',
-            'order[note]' => '']);
+            'order[note]'      => '', ]);
         $this->assertResponseStatusCodeSame(500);
-        
+
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
         $form = $crawler->selectButton('order[submit]')->form();
         $client->submit($form, [
             'order[orderedby]' => '',
-            'order[note]' => '+ Pommes']);
+            'order[note]'      => '+ Pommes', ]);
         $this->assertResponseStatusCodeSame(500);
     }
 }
