7bbe66c0c9c64805adeed89f9e0a698c67534ed1
Sven Nolting <Rindula@users.noreply.github.com>
2021-12-20 22:20:08 +0100

    Add Authentication in tests

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use SecurityTrait;
 
 class OrderTest extends WebTestCase
 {
@@ -10,6 +11,45 @@ class OrderTest extends WebTestCase
     {
         $client = static::createClient();
 
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => 'Max Mustermann',
+            'order[note]'      => '', ]);
+            $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => 'Max Mustermann',
+            'order[note]'      => '+ Pommes', ]);
+            $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => '',
+            'order[note]'      => '', ]);
+            $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => '',
+            'order[note]'      => '+ Pommes', ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $this->loginAs("ROLE_USER", true);
+
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
@@ -45,5 +85,7 @@ class OrderTest extends WebTestCase
             'order[orderedby]' => '',
             'order[note]'      => '+ Pommes', ]);
         $this->assertResponseStatusCodeSame(500);
+
+        $this->logout();
     }
 }
