041534785fb6cedd18f795e1c73bd2a64f22ee22
Sven Nolting <Rindula@users.noreply.github.com>
2021-12-21 09:21:38 +0000

    Apply fixes from StyleCI

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,7 +5,6 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 
 class UserFixture extends Fixture
 {
