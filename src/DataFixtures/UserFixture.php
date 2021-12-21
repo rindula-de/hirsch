3fa1f4b46d88d85cb788232e2ad3d6bb3ade6beb
Sven Nolting <nolting@hochwarth-it.de>
2021-12-21 10:24:53 +0100

    Adapt user fixture

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,15 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
         $user->setUsername('test');
         $user->setRoles(['ROLE_USER']);
-        $user->setPassword($this->container->get('security.password_encoder')->encodePassword($user, 'test'));
+        $user->setPassword($this->hasher->hashPassword($user, 'test'));
 
         $manager->persist($user);
         $manager->flush();
