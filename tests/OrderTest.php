de701ad5393473fc84a20b7efb4cd49de2272dee
Sven Nolting <nolting@hochwarth-it.de>
2021-12-21 10:21:27 +0100

    Fix test authentication

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -2,53 +2,18 @@
 
 namespace App\Tests;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class OrderTest extends WebTestCase
 {
-    use SecurityTrait;
-
-    public function testOrdering(): void
+    public function testOrderingAuthenticated(): void
     {
         $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByUsername('test');
 
-        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
-        $form = $crawler->selectButton('order[submit]')->form();
-        $client->submit($form, [
-            'order[orderedby]' => 'Max Mustermann',
-            'order[note]'      => '', ]);
-        $this->assertResponseStatusCodeSame(401);
-
-        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
-        $form = $crawler->selectButton('order[submit]')->form();
-        $client->submit($form, [
-            'order[orderedby]' => 'Max Mustermann',
-            'order[note]'      => '+ Pommes', ]);
-        $this->assertResponseStatusCodeSame(401);
-
-        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
-        $form = $crawler->selectButton('order[submit]')->form();
-        $client->submit($form, [
-            'order[orderedby]' => '',
-            'order[note]'      => '', ]);
-        $this->assertResponseStatusCodeSame(401);
-
-        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
-        $form = $crawler->selectButton('order[submit]')->form();
-        $client->submit($form, [
-            'order[orderedby]' => '',
-            'order[note]'      => '+ Pommes', ]);
-        $this->assertResponseStatusCodeSame(401);
-
-        $this->login();
+        $client->loginUser($user);
 
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
@@ -85,7 +50,46 @@ class OrderTest extends WebTestCase
             'order[orderedby]' => '',
             'order[note]'      => '+ Pommes', ]);
         $this->assertResponseStatusCodeSame(500);
+    }
 
-        $this->logout();
+    public function testOrderingUnauthenticated(): void
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => 'Max Mustermann',
+            'order[note]'      => '', ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => 'Max Mustermann',
+            'order[note]'      => '+ Pommes', ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => '',
+            'order[note]'      => '', ]);
+        $this->assertResponseStatusCodeSame(401);
+
+        $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
+        $form = $crawler->selectButton('order[submit]')->form();
+        $client->submit($form, [
+            'order[orderedby]' => '',
+            'order[note]'      => '+ Pommes', ]);
+        $this->assertResponseStatusCodeSame(401);
     }
 }
