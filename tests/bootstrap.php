f1aa830cd3d748cbfa439abaef4b02673f652464
Daniel Heer <heer@hochwarth-it.de>
2022-02-04 12:08:38 +0100

    Fixing Comment Part II

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-$_ENV["APP_ENV"] = "test"; //Needed for infection Testing because it sets the env variable to late
+$_ENV["APP_ENV"] = 'test'; //Needed for infection Testing because it sets the env variable to late
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
