b5b7703ed021d79d3fb1c9cb7ce41e59ad46805c
Sven Nolting <Rindula@users.noreply.github.com>
2022-02-16 11:50:14 +0100

    Fix time parsing (#671)
    * Fix time parsing
    
    * Fix unittests
    
    * Apply fixes from StyleCI
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -6,7 +6,9 @@
 
 namespace App\Tests;
 
+use App\Controller\OrderController;
 use App\Repository\UserRepository;
+use Symfony\Bridge\PhpUnit\ClockMock;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -14,8 +16,29 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class OrderTest extends WebTestCase
 {
-    public function testOrderingAuthenticated(): void
+    public function testOrderingAuthenticatedTooLate(): void
     {
+        $this->markAsRisky();
+        ClockMock::register(OrderController::class);
+        ClockMock::withClockMock(strtotime('12:00'));
+        $client = static::createClient([], ['REMOTE_ADDR' => '1.1.1.1']);
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByUsername('test');
+        if ($user === null) {
+            $this->fail('No user found with username "test"');
+        }
+
+        $client->loginUser($user);
+        $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
+        $this->assertResponseRedirects('/karte', 302);
+        ClockMock::withClockMock(false);
+    }
+
+    public function testOrderingAuthenticatedInTime(): void
+    {
+        $this->markAsRisky();
+        ClockMock::register(OrderController::class);
+        ClockMock::withClockMock(strtotime('08:00'));
         $client = static::createClient([], ['REMOTE_ADDR' => '1.1.1.1']);
         $userRepository = static::getContainer()->get(UserRepository::class);
         $user = $userRepository->findOneByUsername('test');
@@ -59,6 +82,7 @@ class OrderTest extends WebTestCase
             'order[orderedby]' => '',
             'order[note]'      => '+ Pommes', ]);
         $this->assertResponseStatusCodeSame(422);
+        ClockMock::withClockMock(false);
     }
 
     public function testOrderingUnauthenticated(): void
