7ce3575681d9efaae5a41364c8f7e181b30a9bb2
Daniel Heer <heer@hochwarth-it.de>
2022-02-21 15:33:12 +0100

    added tests for changelog modal

--- tests/Controller/ModalControllerTest.php
+++ tests/Controller/ModalControllerTest.php
@@ -7,17 +7,30 @@
 namespace App\Tests\Controller;
 
 use App\Entity\Holidays;
+use App\Tests\TestHelper\TestCurlHttpClient;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
+use Psr\Cache\InvalidArgumentException;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\HttpClient\TraceableHttpClient;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 class ModalControllerTest extends WebTestCase
 {
     private EntityManager $entityManager;
     private KernelBrowser $client;
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws InvalidArgumentException
+     */
     protected function setUp(): void
     {
         parent::setUp();
@@ -26,6 +39,16 @@ class ModalControllerTest extends WebTestCase
         $entityManager = $this->getContainer()->get('doctrine')->getManager();
         $this->entityManager = $entityManager;
         $this->entityManager->beginTransaction();
+
+        // prepare environment
+        $cache = new FilesystemAdapter();
+        $cache->clear();
+
+        /** @var Stopwatch $stopwatch */
+        $stopwatch = $this->getContainer()->get('debug.stopwatch');
+        $httpCurlClient = new TestCurlHttpClient($this->getContainer(), "https://api.github.com/repos/Rindula/hirsch/");
+        $httpClient = new TraceableHttpClient($httpCurlClient,$stopwatch);
+        $this->getContainer()->set('http_client',$httpClient);
     }
 
     /**
@@ -62,9 +85,73 @@ class ModalControllerTest extends WebTestCase
         $this->assertEmpty($this->client->getResponse()->getContent());
     }
 
+    /**
+     * @throws \Exception
+     */
+    public function testOneNewRelease(): void
+    {
+        $content = $this->callReleaseModal("v2.7.0");
+        if(!$content)throw new \Exception("Not Content");
+
+        $this->assertStringContainsString("v2.8.0",$content);
+        $this->assertStringNotContainsString("v2.7.0",$content);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testMultipleNewReleases(): void
+    {
+        $content = $this->callReleaseModal("v2.6.0");
+        if(!$content)throw new \Exception("Not Content");
+
+        $this->assertStringContainsString("v2.8.0",$content);
+        $this->assertStringContainsString("v2.7.0",$content);
+        $this->assertStringNotContainsString("v2.6.0",$content);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testNoNewRelease():void
+    {
+        $content = $this->callReleaseModal("v2.8.0");
+
+        $this->assertEmpty($content);
+    }
+
+
+
     protected function tearDown(): void
     {
-        $this->entityManager->rollback();
+        if($this->entityManager->getConnection()->isTransactionActive())$this->entityManager->rollback();
         parent::tearDown();
+    }
+
+    /**
+     * @param string $version
+     * @return false|string
+     * @throws \Exception
+     */
+    private function callReleaseModal(string $version): string|false
+    {
+        $_ENV['APP_VERSION'] = "v2.8.0";
+        $this->client->getCookieJar()->set(new Cookie('changelogVersion', $version, httponly: false));
+        $this->client->request('GET', '/modalChangelog');
+        $this->assertResponseIsSuccessful();
+        $cookies = $this->client->getResponse()->headers->getCookies();
+        $cookieValue = null;
+        foreach ($cookies as $cookie){
+            if($cookie->getName()=="changelogVersion"){
+                $cookieValue = $cookie->getValue();
+                break;
+            }
+        }
+        if($cookieValue === null) throw new \LogicException("Cookie not found");
+        $this->assertEquals($_ENV['APP_VERSION'],$cookieValue);
+        $content = file_get_contents(__DIR__ . "/../mockedApiRequestResponse/releases.page=0");
+        if (!$content) throw new \Exception("Error in test");
+
+        return $this->client->getResponse()->getContent();
     }
 }
---
+++ tests/TestHelper/TestMockResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Tests\TestHelper;
+
+
+use Symfony\Component\HttpClient\Response\MockResponse;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+
+class TestMockResponse extends MockResponse
+{
+
+    private string $body;
+
+    /**
+     * TestMockResponse constructor.
+     * @param string $body
+     * @param array<string,mixed> $info
+     */
+    public function __construct(string $body = '', array $info = [])
+    {
+        parent::__construct($body, $info);
+        $this->body = $body;
+    }
+
+    /**
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws \Exception
+     * @return array<string,mixed>;
+     */
+    public function toArray(bool $throw = true): array
+    {
+        $content = json_decode($this->body);
+        if(!is_array($content))throw new \Exception("Can not convert content to array");
+        foreach ($content as $key=>$value){
+            $content[$key] = $this->objectToArray($value);
+        }
+        return $content;
+    }
+
+    /**
+     * @param \stdClass $object
+     * @return array<string,mixed>
+     */
+    private function objectToArray(\stdClass $object): array
+    {
+        $object = (array) $object;
+        foreach ($object as $key=>$value){
+            if($value instanceof \stdClass)$object[$key]=$this->objectToArray($value);
+        }
+        return $object;
+    }
+}
