54e386c5e6170a38d03e176616090cda8868f91a
Sven Nolting <nolting@hochwarth-it.de>
2022-02-23 09:37:03 +0100

    Coverage => 26,64%

--- tests/Controller/OrderTest.php
+++ tests/Controller/OrderTest.php
@@ -9,6 +9,7 @@ namespace App\Tests\Controller;
 use App\Controller\OrderController;
 use App\Repository\UserRepository;
 use Symfony\Bridge\PhpUnit\ClockMock;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -16,19 +17,24 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class OrderTest extends WebTestCase
 {
-    public function testOrderingAuthenticatedTooLate(): void
+    private function loggedInClient(): KernelBrowser
     {
-        $this->markAsRisky();
-        ClockMock::register(OrderController::class);
-        ClockMock::withClockMock(strtotime('12:00'));
         $client = static::createClient([], ['REMOTE_ADDR' => '1.1.1.1']);
         $userRepository = static::getContainer()->get(UserRepository::class);
         $user = $userRepository->findOneByUsername('test');
         if ($user === null) {
             $this->fail('No user found with username "test"');
         }
-
         $client->loginUser($user);
+        return $client;
+    }
+
+    public function testOrderingAuthenticatedTooLate(): void
+    {
+        $this->markAsRisky();
+        ClockMock::register(OrderController::class);
+        ClockMock::withClockMock(strtotime('12:00'));
+        $client = $this->loggedInClient();
         $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseRedirects('/karte', 302);
         ClockMock::withClockMock(false);
@@ -39,14 +45,7 @@ class OrderTest extends WebTestCase
         $this->markAsRisky();
         ClockMock::register(OrderController::class);
         ClockMock::withClockMock(strtotime('08:00'));
-        $client = static::createClient([], ['REMOTE_ADDR' => '1.1.1.1']);
-        $userRepository = static::getContainer()->get(UserRepository::class);
-        $user = $userRepository->findOneByUsername('test');
-        if ($user === null) {
-            $this->fail('No user found with username "test"');
-        }
-
-        $client->loginUser($user);
+        $client = $this->loggedInClient();
         $crawler = $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Schweizer Wurstsalat mit Pommes');
@@ -92,4 +91,29 @@ class OrderTest extends WebTestCase
         $client->request('GET', '/order/0/Schweizer-Wurstsalat-mit-Pommes');
         $this->assertResponseStatusCodeSame(401);
     }
+
+    public function testOrderingInvalidOrderSlug(): void
+    {
+        $client = $this->loggedInClient();
+        $client->request('GET', '/order/0/sngerjuioernruioesbnioernb');
+        $this->assertResponseStatusCodeSame(302);
+    }
+
+    public function testOrderUntil(): void
+    {
+        $client = $this->loggedInClient();
+        $client->request('GET', '/order-until');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('', 'Bestellungen am selben Tag bis 10:55 möglich');
+    }
+
+    public function testOrdersOverview(): void
+    {
+        $client = $this->loggedInClient();
+        $client->request('GET', '/bestellungen/');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Heutige Bestellungen');
+    }
+
+
 }
