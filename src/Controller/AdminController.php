0e8684783b6c196f9ee07a8a810c2d07d6dd63b1
Sven Nolting <nolting@hochwarth-it.de>
2022-12-01 11:47:16 +0100

    Deaktivierung via Cache
    also fixes #1162

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,8 +7,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
 
 #[Route('/admin')]
 class AdminController extends AbstractController
@@ -19,5 +21,21 @@ class AdminController extends AbstractController
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
+    }
+
+    #[Route('/holidays', name: 'app_admin_disable_menu')]
+    public function disableMenu(): Response
+    {
+        $cache = new FilesystemAdapter();
+        $cache->deleteItem('menu_disabled');
+
+        $cache->get('menu_disabled', function (ItemInterface $item) {
+            $item->expiresAt(new \DateTime('tomorrow midnight'));
+            return true;
+        });
+
+        $this->addFlash('success', 'admin.notification.menu_disabled');
+
+        return $this->redirectToRoute('app_admin_index');
     }
 }
