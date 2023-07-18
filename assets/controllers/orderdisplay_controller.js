1ad073b98d709da83a38d56abec66b5b0446d1d8
Sven Nolting <nolting@hochwarth-it.de>
2023-07-18 09:49:16 +0200

    Use native symfony ux for controller

--- assets/controllers/orderdisplay_controller.js
+++ assets/controllers/orderdisplay_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "stimulus";
 import $ from "jquery";
 
 export default class extends Controller {
+    static values = {
+        name: String,
+        ordered: String,
+        special: String
+    }
+
     connect() {
         $(this.element).click(function() {
-            $('#orderedModalTitle').text('Bestellung von '+$(this.element).attr('data-name'));
-            $('#orderedModalText').text($(this.element).attr('data-ordered'));
-            if ($(this.element).attr('data-special')!='') {
+            $('#orderedModalTitle').text('Bestellung von '+this.nameValue);
+            $('#orderedModalText').text(this.orderedValue);
+            if (this.specialValue!='') {
                 $('#orderedModalText').append('<br><br>')
-                $('#orderedModalText').append(document.createTextNode($(this.element).attr('data-special')));
+                $('#orderedModalText').append(document.createTextNode(this.specialValue));
             }
             $('#orderedModal').addClass('active');
         }.bind(this));
