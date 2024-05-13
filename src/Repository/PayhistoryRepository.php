29cbbb582e290cb2652e73016656f39806e54fdf
Sven Nolting <Rindula@users.noreply.github.com>
2024-05-13 16:41:11 +0200

    Use getEntityManager() instead of property $_em

--- src/Repository/PayhistoryRepository.php
+++ src/Repository/PayhistoryRepository.php
@@ -58,9 +58,9 @@ class PayhistoryRepository extends ServiceEntityRepository
      */
     public function add(Payhistory $entity, bool $flush = true): void
     {
-        $this->_em->persist($entity);
+        $this->getEntityManager()->persist($entity);
         if ($flush) {
-            $this->_em->flush();
+            $this->getEntityManager()->flush();
         }
     }
 
@@ -70,9 +70,9 @@ class PayhistoryRepository extends ServiceEntityRepository
      */
     public function remove(Payhistory $entity, bool $flush = true): void
     {
-        $this->_em->remove($entity);
+        $this->getEntityManager()->remove($entity);
         if ($flush) {
-            $this->_em->flush();
+            $this->getEntityManager()->flush();
         }
     }
 
